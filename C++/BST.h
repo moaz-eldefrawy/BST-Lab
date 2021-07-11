8bc3c6972dd683ab76b55de6aedbc1abf0d62720
ABE-Mark45 <ahmedinmasr@gmail.com>
2021-07-11 21:54:46 +0200

    C++ added

---
+++ C++/BST.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <vector>
+#include "fruits.h"
+
+void output_vector(std::vector<std::pair<FruitType, int>>&);
+
+class BST
+{
+private:
+    BSTNode* root;
+    void Iterate(BSTNode *node);
+    void filterByType(BSTNode* node, FruitType type, std::vector<std::pair<FruitType, int>>& res);
+    void filterByWeight(BSTNode* node, int weight, std::vector<std::pair<FruitType, int>>& res);
+
+    BSTNode* insertFruit(BSTNode* node, FruitType type, int weight);
+    BSTNode* findLightest(BSTNode* node);
+    BSTNode* deleteFruit(BSTNode* node, FruitType type, int weight);
+
+public:
+    void insertFruit(FruitType type, int weight);
+    void deleteFruit(FruitType type, int weight);
+    BST() : root(nullptr) {}
+    void Iterate();
+    std::vector<std::pair<FruitType, int>> filterByType(FruitType type);
+    std::vector<std::pair<FruitType, int>> filterByWeight(int weight);
+    void magnifyByType(FruitType type, int weight);
+    BSTNode* findHeaviest();
+    BSTNode* findLightest();
+};
---
+++ C++/fruits.h
@@ -0,0 +1,139 @@
+#pragma once
+
+enum FruitType
+{
+    APPLE,
+    OVAL_SHAPED,
+    AVOCADO,
+    TINY_FRUIT,
+    GRAPES,
+    BERRIES,
+    ELDER_BERRIES,
+    GOOSE_BERRIES,
+    BLUE_BERRIES
+};
+
+class BSTNode
+{
+public:
+    int weight;
+    BSTNode *left;
+    BSTNode *right;
+    BSTNode() : left(nullptr), right(nullptr) {}
+    BSTNode(int weight) : weight(weight), left(nullptr), right(nullptr) {}
+    virtual bool isType(FruitType _type) = 0;
+    virtual FruitType getType() = 0;
+};
+
+class OvalShaped : public BSTNode
+{
+public:
+    OvalShaped(int weight) : BSTNode(weight) {}
+
+    virtual bool isType(FruitType type)
+    {
+        return type == OVAL_SHAPED;
+    }
+};
+
+class Apple : public OvalShaped
+{
+public:
+    Apple(int weight) : OvalShaped(weight) {}
+
+    virtual FruitType getType()
+    {
+        return APPLE;
+    }
+
+    virtual bool isType(FruitType type)
+    {
+        return type == APPLE || OvalShaped::isType(type);
+    }
+};
+
+class Avocado : public OvalShaped
+{
+public:
+    Avocado(int weight) : OvalShaped(weight) {}
+
+    virtual FruitType getType()
+    {
+        return AVOCADO;
+    }
+
+    virtual bool isType(FruitType type)
+    {
+        return type == AVOCADO || OvalShaped::isType(type);
+    }
+};
+
+class TinyFruit : public BSTNode
+{
+public:
+    TinyFruit(int weight) : BSTNode(weight) {}
+
+    virtual bool isType(FruitType type)
+    {
+        return type == TINY_FRUIT;
+    }
+};
+
+class Berries : public TinyFruit
+{
+public:
+    Berries(int weight) : TinyFruit(weight) {}
+
+    virtual bool isType(FruitType type)
+    {
+        return type == BERRIES || TinyFruit::isType(type);
+    }
+};
+
+class Grapes : public TinyFruit
+{
+public:
+    Grapes(int weight) : TinyFruit(weight) {}
+
+    virtual FruitType getType()
+    {
+        return GRAPES;
+    }
+
+    virtual bool isType(FruitType type)
+    {
+        return type == GRAPES || TinyFruit::isType(type);
+    }
+};
+
+class BlueBerries : public Berries
+{
+public:
+    BlueBerries(int weight) : Berries(weight) {}
+
+    virtual FruitType getType()
+    {
+        return BLUE_BERRIES;
+    }
+
+    virtual bool isType(FruitType type)
+    {
+        return type == BLUE_BERRIES || Berries::isType(type);
+    }
+};
+
+class GooseBerries : public Berries
+{
+public:
+    GooseBerries(int weight) : Berries(weight) {}
+
+    virtual FruitType getType()
+    {
+        return GOOSE_BERRIES;
+    }
+
+    virtual bool isType(FruitType type)
+    {
+        return type == GOOSE_BERRIES || Berries::isType(type);
+    }
+};
